d67654c14704139e9a97aec4813b044eaa25c926
Doridian <git@doridian.net>
2025-01-10 08:12:26 -0800

    fix

--- restic.sh
+++ restic.sh
@@ -11,4 +11,4 @@ host="$(hostname)"
 
 #runx "s3:s3.us-west-001.backblazeb2.com/${host}-backups" "$@"
 REST_SERVER_PASSWORD="$(cat /mnt/keydisk/rest-server-password | tr -d '\r\n\t ')"
-runx "rest:https://bengalfox:$REST_SERVER_PASSWORD@restserver.foxden.network/${host}/main" "$@"
+runx "rest:https://${host}:${REST_SERVER_PASSWORD}@restic.foxden.network/${host}/main" "$@"
