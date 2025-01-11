63fd66a2db84033b59f4c00f5fb85f7554308186
Doridian <git@doridian.net>
2025-01-10 18:10:39 -0800

    Add slice

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 ln -sf /opt/endpoint/systemd.timer /etc/systemd/system/opt-endpoint-backup.timer
 ln -sf /opt/endpoint/systemd.service /etc/systemd/system/opt-endpoint-backup.service
+ln -sf /opt/endpoint/systemd.slice /etc/systemd/system/opt-endpoint-backup.slice
+systemctl daemon-reload
 systemctl enable --now opt-endpoint-backup.timer
