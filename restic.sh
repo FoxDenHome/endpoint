56dd1eb317ce610af336ef69102520dfc004534a
Doridian <git@doridian.net>
2025-02-23 14:14:48 -0800

    Stop using $REPO

--- restic.sh
+++ restic.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 runx() {
-    export REPO="$1"
-    export CMD="$2"
-    shift 2
+    export CMD="$1"
+    shift 1
     "$CMD" "$@"
 }
 
 host="$(hostname)"
 
-#runx "s3:s3.us-west-001.backblazeb2.com/${host}-backups" "$@"
+#export RESTIC_REPOSITORY="s3:s3.us-west-001.backblazeb2.com/${host}-backups"
+#runx "$@"
+
 REST_SERVER_PASSWORD="$(cat /mnt/keydisk/rest-server-password | tr -d '\r\n\t ')"
-runx "rest:https://${host}:${REST_SERVER_PASSWORD}@restic.foxden.network/${host}/main" "$@"
+export RESTIC_REPOSITORY="rest:https://${host}:${REST_SERVER_PASSWORD}@restic.foxden.network/${host}/main"
+runx "$@"
