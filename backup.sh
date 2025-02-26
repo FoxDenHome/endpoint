eaa883245f8180f4c60980437f745305c2eb7eed
Doridian <git@doridian.net>
2025-02-26 15:57:38 -0800

    aaa

--- backup.sh
+++ backup.sh
@@ -5,10 +5,8 @@ if /opt/endpoint/is-on-battery.sh; then
 	exit 0
 fi
 
-BACKUP_EXTRADIRS=/efi
-
 if [ -f /opt/endpoint/local.sh ]; then
 	. /opt/endpoint/local.sh
 fi
 
-/usr/bin/systemd-inhibit --no-ask-password --what='idle:sleep' --why='restic backup' --no-pager /opt/endpoint/restic.sh /opt/backup/run.sh / $BACKUP_EXTRADIRS
+/usr/bin/systemd-inhibit --no-ask-password --what='idle:sleep' --why='restic backup' --no-pager /opt/endpoint/restic.sh /opt/backup/run.sh / /efi
