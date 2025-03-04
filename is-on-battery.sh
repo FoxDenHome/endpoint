089d9e9c5f3a18271abed545f4810dcefdae80a1
Doridian <git@doridian.net>
2025-03-03 22:32:04 -0800

    Hide silly

--- is-on-battery.sh
+++ is-on-battery.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for stat in `cat /sys/class/power_supply/BAT*/status`; do
+for stat in `cat /sys/class/power_supply/BAT*/status 2>/dev/null`; do
 	if [ "$stat" = "Discharging" ]; then
 		exit 0
 	fi
