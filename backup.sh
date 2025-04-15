49ff553452cc0657b6b27cf7744aa10e7a893560
Doridian <git@doridian.net>
2025-04-14 17:08:01 -0700

    undo

--- backup.sh
+++ backup.sh
@@ -9,9 +9,4 @@ if [ -f /opt/endpoint/local.sh ]; then
 	. /opt/endpoint/local.sh
 fi
 
-/usr/bin/systemd-inhibit --no-ask-password --what='idle:sleep' --why='restic backup' --no-pager /opt/endpoint/restic.sh /opt/backup/run.sh / /efi
-_exit=$?
-if [ $_exit -eq 3 ]; then
-	_exit=0
-fi
-exit $_exit
+exec /usr/bin/systemd-inhibit --no-ask-password --what='idle:sleep' --why='restic backup' --no-pager /opt/endpoint/restic.sh /opt/backup/run.sh / /efi
